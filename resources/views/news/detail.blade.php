6bd21c4b28807483e5050fcf7032685d50788a96
chieh0666 <jerry970246@gmail.com>
2025-04-18 00:29:43 +0800

    Refactor code structure for improved readability and maintainability

--- resources/views/news/detail.blade.php
+++ resources/views/news/detail.blade.php
@@ -8,12 +8,18 @@
   <h2 class="py-1875">
     <span class="border-start border-top ps-2 pt-1 border-3">{{ $page_title }}</span>
   </h2>
+  <span class="fs-6">
+    {{ $news->created_at->diffForHumans() }}
+  </span>
 </div>
 
 <article id="news-detail-page">
-  {{ $news->created_at->diffForHumans() }}
-  <img src="/{{ $news->image }}" alt="">
-  <p>{{ $news->content }}</p>
+  <div class="mx-auto col-md-10 col-12">
+    <div class="mb-3">
+      <img class="rounded-2" src="/{{ $news->image }}" alt="{{ $news->title }}">
+    </div>
+    <p>{!! $news->content !!}</p>
+  </div>
 </article>
 
 @endsection
