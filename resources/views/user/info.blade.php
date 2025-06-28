8e3f02bf598cfe8e9bd6d77459d41339df1d013e
chieh0666 <jerry970246@gmail.com>
2025-06-29 04:04:45 +0800

    feat: format user information display in profile view

--- resources/views/user/info.blade.php
+++ resources/views/user/info.blade.php
@@ -7,13 +7,14 @@
 <h2 class="h5 m-3">個人資訊</h2>
 
 <div class="mx-3 my-2">
-  {{ $user->last_name }}
-  {{ $user->first_name }}
-  {{ $user->email }}
-  {{ $user->phone }}
-  {{ $user->photo }}
-  {{ $user->gender }}
-  {{ $user->nickname }}
-  加入日期：{{ $user->created_at }}
+  <ul>
+    <li>姓氏：{{ $user->last_name }}</li>
+    <li>名字：{{ $user->first_name }}</li>
+    <li>電子郵件：{{ $user->email }}</li>
+    <li>電話：{{ $user->phone }}</li>
+    <li>照片：{{ $user->photo }}</li>
+    <li>性別：{{ $user->gender }}</li>
+    <li>暱稱：{{ $user->nickname }}</li>
+  </ul>
 </div>
 @endsection
